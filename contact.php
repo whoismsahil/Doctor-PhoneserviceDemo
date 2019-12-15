9fb8c419fa2dad9326c1445ce100a2fba5ff6bdd
Mohammad Sahil <warior.mohammadsahil@gmail.com>
2019-12-15 16:10:43 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,7 +5,7 @@
     $address = $_POST['message'];
 
     $email_from = 'OrderPlaced_GZP@doctor_phoneservice.com';
-    $email_subject = "$name Order Placed";
+    $email_subject = "New Order Placed";
     $email_body = "Full Name: $name.\n".
                     "Mobile Number: $visitor_number.\n".
                         "Address: $address.\n";
@@ -13,7 +13,7 @@
     $to = "warior.mohammadsahil@gmail.com";
 
     $headers = "From: $email_from \r\n";
-    $headers .= "Reply-To: $visitor_number \r\n";
+    $headers .= "Reply-To: $visitor_email \r\n";
 
     mail($to,$email_subject,$email_body,$headers);
 
